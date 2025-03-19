bf87d15c9702330336cf7a316fb28ff955af1945
jkosticova <jana.kosticova@gmail.com>
2025-03-19 08:28:55 +0100

    Move router to App.jsx to follow recommended pattern

--- not-twitter-fe/src/App.jsx
+++ not-twitter-fe/src/App.jsx
@@ -2,16 +2,14 @@
 import Header from './components/Header';
 import { getMessages } from './services/messageService'
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MessageListPage from './pages/MessageListPage';
 import NewMessagePage from './pages/NewMessagePage';
 
 function App() {
 
-  const [messages, setMessages] = useState([]);    
-  
-  const location = useLocation();  
- 
+  const [messages, setMessages] = useState([]);  
+
   // periodically refresh (timer)
   useEffect(() => {
     setMessages(getMessages());
@@ -24,10 +22,21 @@ function App() {
   return (
     <div className="container">
       <Header messages={messages} />
-      {location.pathname === "/" && <MessageListPage messages={messages}  />}
-      {location.pathname === "/compose" && <NewMessagePage setMessages={setMessages} />}         
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<MessageListPage messages={messages} />}
+          />
+          <Route
+            path="/compose"
+            element={<NewMessagePage setMessages={setMessages} />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
+
 
 }
 
--- not-twitter-fe/src/main.jsx
+++ not-twitter-fe/src/main.jsx
@@ -2,23 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css'
 
-const router = createBrowserRouter([
-  {    
-    path: "/",
-    element: <App />    
-  },
-  {
-    path: "/compose",
-    element: <App />    
-  }
-])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <RouterProvider router={router} />
+     <App />
   </StrictMode>
 )
