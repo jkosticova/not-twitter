91779b58822c73a400ef03047d9cc4f84c87ea27
jkosticova <jana.kosticova@gmail.com>
2025-03-26 13:11:26 +0100

    Add querying messages from DB in backend

---
+++ not-twitter-be/models/messages.js
@@ -0,0 +1,23 @@
+const {Pool} = require('pg');
+const {config} = require('../config.secrets')
+
+const pool = new Pool({
+    user: config.db.user,
+    host: config.db.host,
+    database: config.db.database,
+    password: config.db.password,
+    port: config.db.port,
+  });
+
+exports.getMessages = function() {
+    return pool.query(`
+        select m.*, u.avatar
+        from messages m
+        left join users u on m.user_id=u.id
+    `);
+};
+
+exports.addMessage = function(message) {
+    return pool.query("insert into messages(id, user_id, text) values($1, $2, $3)", 
+[message.id, message.user_id, message.text]);    
+};
