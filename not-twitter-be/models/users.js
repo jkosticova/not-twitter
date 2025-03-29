74355d4c694fac6ff5c86f92b96c0737441dab01
jkosticova <jana.kosticova@gmail.com>
2025-03-29 12:54:25 +0100

    Add quering user's hashed password from DB / backend

--- not-twitter-be/models/users.js
+++ not-twitter-be/models/users.js
@@ -1,7 +1,6 @@
 var pool = require('../config/db.js');
-var { comparePassword } = require ('../utils/authHelpers.js');
 
-exports.getHashedPassword = function(username) {
+exports.getHashedPassword = function(username) {   
     return pool.query(
         "select password from users u where u.username = $1",
         [username]
