895081c8796d4ff11ae5b374c147cc6d31eb5365
jkosticova <jana.kosticova@gmail.com>
2025-03-29 13:00:51 +0100

    Create backend endpoint for login

--- not-twitter-be/routes/api_v1/auth.js
+++ not-twitter-be/routes/api_v1/auth.js
@@ -1,31 +1,38 @@
 var express = require('express'); // ESM: import
 var { getHashedPassword } = require('../../models/users')
-var { comparePassword } = require('../utils/authHelpers.js');
+var { comparePassword } = require('../../utils/authHelpers.js');
 var router = express.Router();
 
-router.post("/", (req, res) => {
+router.post("/login", (req, res) => {
     const { username, password } = req.body;
     getHashedPassword(username)
         .then((result) => {
             if (result.rows.length === 1) {
-                if (comparePassword(password, result.password)) {
-                    // TODO: create session
-                    res.status(200).json({ message: "Login successful" });
-                }
-                // invalid password
-                else {
-                    res.status(401).json({ error: "Invalid credentials" });
-                }
+                comparePassword(password, result.rows[0].password)
+                    .then((isValid) => {
+                        if (isValid) {
+                            req.session.user = { username };  // creates session
+                            console.log("Login successful");                            
+                            return res.status(200).end();
+                        }
+                        // invalid password
+                        else {
+                            console.log("Invalid password");
+                            return res.status(401).end();
+                        }
+                    }).catch((e) => { 
+                        console.log(e); 
+                        res.status(500).end(); 
+                    })
             }
             // user does not exist
             else {
-                res.status(401).json({ error: "Invalid credentials" });
+                console.log("User does not exist");
+                return res.status(401).end();
             }
-        })
-        // another error
-        .catch((e) => {
+        }).catch((e) => {
             console.log(e);
-            res.status(500);
+            return res.status(500).end();
         })
 });
 
