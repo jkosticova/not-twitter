d296cb75c283ee8035ecde61ff74033419e987be
jkosticova <jana.kosticova@gmail.com>
2025-03-30 16:12:50 +0200

    Display username for each message / frontend

--- not-twitter-fe/src/components/Message.jsx
+++ not-twitter-fe/src/components/Message.jsx
@@ -1,18 +1,27 @@
 function Message(props) {
-    
+
     return (
-        <div className="row mb-3 mt-3">
-            <div className="col-sm-1">
-                <div className="py-1">
-                    <img src={props.message.avatar} className="fixed-size-avatar"></img>
+        <>
+            <div className="row mb-3 mt-3">
+                <div className="col-sm-1">
+                    <div className="py-1">
+                        <img src={props.message.avatar} className="fixed-size-avatar"></img>
+                    </div>
+                </div>
+                <div className="col-sm-1">
+                    <div className="py-1">
+                        {props.message.username}
+                    </div>
                 </div>
             </div>
-            <div className="col-sm">
-                <div className="py-2">
-                    {props.message.text}
+            <div className="row mb-3 mt-3">
+                <div className="col-sm">
+                    <div className="py-2">
+                        {props.message.text}
+                    </div>
                 </div>
-            </div>          
-        </div>
+            </div>
+        </>
     )
 }
 
