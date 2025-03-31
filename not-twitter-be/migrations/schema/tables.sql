eaa3e27b2a3cc6a4aad396009de3312aa798d464
jkosticova <jana.kosticova@gmail.com>
2025-03-31 13:41:18 +0200

    Fix constraints in create table statements / DB

--- not-twitter-be/migrations/schema/tables.sql
+++ not-twitter-be/migrations/schema/tables.sql
@@ -11,8 +11,9 @@ CREATE TABLE "public"."messages" (
     "author" varchar(100) NOT NULL,
     "text" text NOT NULL,
     PRIMARY KEY ("message_id"),
-    CONSTRAINT "messages_user_fk" FOREIGN KEY ("author") 
-REFERENCES "public"."users" ("user_id") ON DELETE CASCADE
+    CONSTRAINT "fk_messages_author_user_id" FOREIGN KEY ("author") 
+    REFERENCES "public"."users" ("user_id") 
+    ON DELETE CASCADE
 );
 
 CREATE TABLE "public"."friends" (    
@@ -20,7 +21,11 @@ CREATE TABLE "public"."friends" (
     "user2_id" varchar(100) NOT NULL,
     PRIMARY KEY ("user1_id", "user2_id"),
     CONSTRAINT "user_user_fk1" FOREIGN KEY ("user1_id") REFERENCES "public"."users" ("user_id") ON DELETE CASCADE, 
-    CONSTRAINT "user_user_fk2" FOREIGN KEY ("user2_id") REFERENCES "public"."users" ("user_id") ON DELETE CASCADE 
+    CONSTRAINT "user_user_fk2" FOREIGN KEY ("user2_id") REFERENCES "public"."users" ("user_id") ON DELETE CASCADE, 
+    CONSTRAINT "unique_friendship" CHECK (
+        ("user1_id" < "user2_id") 
+        OR ("user1_id" > "user2_id")
+    )
 );
 
 CREATE TABLE "session" (
@@ -36,5 +41,6 @@ INSERT INTO "public"."users"(user_id, username, password, avatar) VALUES
 INSERT INTO "public"."users"(user_id, username, password, avatar) VALUES 
 ('200','sampleUser2', '$2a$12$.Mj67PkM8s3W6Xyplc.AEu52UscuEfxhHOJIeZAkZy.dAlyYnikKa', 'images/person-circle.svg');
 
+-- sampleUser1 and sampleUser2 are friends
 INSERT INTO "public"."friends" (user1_id, user2_id) VALUES
 ('100', '200');
