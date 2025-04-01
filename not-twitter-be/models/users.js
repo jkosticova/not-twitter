962c406afba47a5fdea7f19703dddf631c03c7fc
jkosticova <jana.kosticova@gmail.com>
2025-04-01 12:37:29 +0200

    Add comments to backend

--- not-twitter-be/models/users.js
+++ not-twitter-be/models/users.js
@@ -1,5 +1,7 @@
 var pool = require('../config/db.js');
 
+// use parametrized queries to prevent SQL injection 
+
 // returns promise !
 exports.getHashedPassword = function(username) {   
     return pool.query(
--- not-twitter-be/routes/api_v1/messages.js
+++ not-twitter-be/routes/api_v1/messages.js
@@ -24,7 +24,6 @@ router.get('/', function (req, res, next) {
     }
 });
 
-
 router.post('/', function (req, res, next) {
     console.log(req.session);
     console.log(req.sessionID);
