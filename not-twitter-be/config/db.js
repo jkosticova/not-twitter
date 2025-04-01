ac5be36ff62f87235c78e17e734123a227a48c36
jkosticova <jana.kosticova@gmail.com>
2025-04-01 18:26:52 +0200

    Fix using env variables in creating db pool

--- not-twitter-be/config/db.js
+++ not-twitter-be/config/db.js
@@ -1,7 +1,14 @@
-const {Pool} = require('pg');
-const { config } = require('./config.js');
+const { Pool } = require('pg');
+require('dotenv').config()
 
-
-const pool = new Pool(config.db);
+const pool = new Pool(
+    {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT
+    }
+);
 
 module.exports = pool; 
